ade3a96ae88e3e8e9a4d163f92af1201bbfe6b42
Martijn van der Ven <martijn@vanderven.se>
2018-12-30 21:08:32 +0100

    Add tests for testing, one always fails!

--- test/StreamFactoryTestCase.php
+++ test/StreamFactoryTestCase.php
@@ -61,4 +61,46 @@ abstract class StreamFactoryTestCase extends TestCase
 
         $this->assertStream($stream, $string);
     }
+
+    public function testCreateStreamTell()
+    {
+        $string = 'would you like some crumpets?';
+
+        $stream = $this->factory->createStream($string);
+
+        $this->assertSame(0, $stream->tell(), 'Tell on Stream from String.');
+        $this->assertSame(29, $stream->tell(), 'Tell on Stream from String.');
+    }
+
+    public function testCreateStreamFromFileTell()
+    {
+        $string = 'would you like some crumpets?';
+        $filename = $this->createTemporaryFile();
+
+        file_put_contents($filename, $string);
+
+        $stream = $this->factory->createStreamFromFile($filename);
+
+        $this->assertSame(0, $stream->tell(), 'Tell on Stream from File.');
+    }
+
+    public function testCreateStreamFromResourceTell()
+    {
+        $string = 'would you like some crumpets?';
+        $halflength = \intdiv(\strlen($string), 2);
+        $resource1 = $this->createTemporaryResource($string);
+        \fseek($resource1, 0, \SEEK_SET);
+        $resource2 = $this->createTemporaryResource($string);
+        \fseek($resource2, $halflength, \SEEK_SET);
+        $resource3 = $this->createTemporaryResource($string);
+        \fseek($resource3, 0, \SEEK_END);
+
+        $stream1 = $this->factory->createStreamFromResource($resource1);
+        $stream2 = $this->factory->createStreamFromResource($resource2);
+        $stream3 = $this->factory->createStreamFromResource($resource3);
+
+        $this->assertSame(0, $stream1->tell(), 'Tell at start of Stream from Resource.');
+        $this->assertSame($halflength, $stream2->tell(), 'Tell halfway through Stream from Resource.');
+        $this->assertSame(29, $stream3->tell(), 'Tell at end of Stream from Resource.');
+    }
 }
